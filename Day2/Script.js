e0523d3ceb5cc1f5e00bb5210fcb161c8e23723b
dineshkumar-stack <stardinesh4@gmail.com>
2023-01-27 12:03:11 +0530

    Added Day2

---
+++ Day2/Script.js
@@ -0,0 +1,15 @@
+a = [1,2,3,4,5,6];
+for(let i of a){
+    if(i%2!=0){
+    continue
+}
+console.log(i);
+}
+
+a = [1,2,3,4,5,6];
+    for(let i of a){
+        if(i%2){
+            continue
+    }
+console.log(i);
+}
